e84308c266461e6925c9706caa8dcbc02271298e
Cdreetz <cdreetz@gmail.com>
2024-01-02 01:16:53 -0600

    more

--- parsing/main_pdf.py
+++ parsing/main_pdf.py
@@ -21,28 +21,28 @@ pdf_url = NSURL.fileURLWithPath_(url)
 pdf_document = Quartz.PDFDocument.alloc().initWithURL_(pdf_url)
 
 number_of_pages = pdf_document.pageCount()
-# extracted_text = ""
-# 
-# 
-# for page in range(number_of_pages):
-#     pdf_page = pdf_document.pageAtIndex_(page)
-#     page_text = pdf_page.string()
-#     extracted_text += page_text
-# 
-# extracted_text = extracted_text.replace('\n', '')
-# tokens = num_tokens(extracted_text, encoding)
-# print(f"Number of tokens: {tokens}")
-# 
-# 
-# completion = oai.chat.completions.create(
-#     model="gpt-4-1106-preview",
-#     messages=[
-#         {"role":"system","content":"You are a helpful assistant."},
-#         {"role":"user","content":f"Please list the top 10 concepts throughout this paper, in pullet point format.\n\n{extracted_text}"},
-#     ]
-# )
-# response = completion.choices[0].message.content
-#print(response)
+extracted_text = ""
+
+
+for page in range(number_of_pages):
+    pdf_page = pdf_document.pageAtIndex_(page)
+    page_text = pdf_page.string()
+    extracted_text += page_text
+
+extracted_text = extracted_text.replace('\n', '')
+tokens = num_tokens(extracted_text, encoding)
+print(f"Number of tokens: {tokens}")
+
+
+completion = oai.chat.completions.create(
+    model="gpt-4-1106-preview",
+    messages=[
+        {"role":"system","content":"You are a helpful assistant."},
+        {"role":"user","content":f"Please list the top 10 concepts throughout this paper, in pullet point format.\n\n{extracted_text}"},
+    ]
+)
+response = completion.choices[0].message.content
+print(response)
 
 def print_page_text(page_number: int):
     if page_number < 0 or page_number >= number_of_pages:
@@ -138,3 +138,4 @@ process_pdf_urls(pdf_urls)
 #     write_json_to_file(summaries, 'summaries.json')
 # 
 # summarize_chunks()
+
